42f5e2b7613fe26c2cf33cf978fbb81ffc324b92
Gwynn <10675768+JeSmyrna@users.noreply.github.com>
2025-10-09 13:05:16 +0200

    updated gsheet function

--- gsheets.py
+++ gsheets.py
@@ -3,21 +3,43 @@ import read_write_csv as rw_csv
 
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-def gspread_overwrite(link:str,row_data:dict):
+def gspread_overwrite(link:str,row_data:dict,start_cell:str = 'A1'):
     gc = gspread.oauth()
     sh = gc.open_by_url(f'{link}')
-    row = len(row_data)
-    worksheet = sh.get_worksheet(0)
+    worksheet = sh.get_worksheet(1)
 
-    list_length = len(row_data['columns']) - 1
+    end_col_row = calc_sheet_length(row_data,start_cell)
     lists = []
 
     for entry in row_data:
         lists.append(row_data[entry])
 
-    worksheet.update(lists, f'A1:{alphabet[list_length].capitalize()}{row}')
+    worksheet.update(lists, f'{start_cell}:{end_col_row}')
 
+#function is here just for testing
 def load_sr_sheet_data(filename:str):
     data = rw_csv.load_sr_sheet(f'{filename}')
         
     return data
+
+def calc_sheet_length( sr_sheet:dict, start_cell:str = 'A1'):
+    start_cell_column = start_cell[0].lower()
+    start_cell_row = int(start_cell[1])
+    #print(f'Starting Cell {start_cell}: {int(alphabet.index(start_cell_column))+1},{start_cell_row}')
+
+    list_length = len(sr_sheet['columns'])-1
+
+    to_column = alphabet.index(start_cell_column)+list_length
+    to_row = len(sr_sheet) + start_cell_row -1
+    #print(f'{alphabet[to_column].capitalize()}{to_row}')
+    end_row_col = f'{alphabet[to_column].capitalize()}{to_row}'
+
+    return end_row_col
+
+def testing_input():
+    entered_url = input('GSheet URL: ')
+    entered_starting_cell = input('Starting Cell (Header:Player -> etc) like "A1": ')
+    data = load_sr_sheet_data('BWL_Test')
+    gspread_overwrite(entered_url,data,entered_starting_cell)
+
+testing_input()
