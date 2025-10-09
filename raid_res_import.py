67b99a3113025642ff3e284f6992529058279390
Gwynn-DevArt <10675768+JeSmyrna@users.noreply.github.com>
2025-10-09 22:31:35 +0200

    fixed bug: last item was short of 1 character

--- raid_res_import.py
+++ raid_res_import.py
@@ -7,10 +7,10 @@ def format_sr_players(user_entry):
     player_list = user_entry.split("[00:00]")
     player_list.pop(0)
     for player in player_list:
-        first_part = player.split(":")
+        first_part = player.split(": ")        
         secnd_part = first_part[1].split(" - ")
         item = secnd_part[1]
-        item = item[:-1]
+        #item = item[:-1]
         if first_part[0] not in raid_res_player_dict:
             raid_res_player_dict[first_part[0]] = item
         else:
@@ -22,12 +22,12 @@ def get_soft_reserve_players():
     with open("raidres.txt") as file_in:
         lines = []
         for line in file_in:
-            lines.append(line)
+            lines.append(line.rstrip('\n'))
         
         for entry in lines:
             format_sr_players(entry)
-
     return raid_res_player_dict
 
+#get_soft_reserve_players()
         #for entry in raid_res_player_dict:
             #print(f"{entry} : {raid_res_player_dict[entry]}")
