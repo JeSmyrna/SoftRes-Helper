146ea172fdf88a04555952a74299ebaba6cdc9c1
Gwynn <10675768+JeSmyrna@users.noreply.github.com>
2025-10-14 11:23:26 +0200

    fixed intersect_raidres_and_attendees func

--- raid_attendance.py
+++ raid_attendance.py
@@ -9,21 +9,21 @@ def get_raid_attendees():
 
     return attendees_list
 
-def intersect_raidres_and_attendees(attendeese:list, raidres:dict) -> dict:
+def intersect_raidres_and_attendees(attendeese:list, raidres:dict) -> tuple[dict,dict]:
     attendees_dict = {}
     not_attended_dict = {}
+
     for player in raidres:
-        
+        items = []
+
         if player in attendeese:
-            items = []
             items = raidres.get(player)
-            
             if items == []:
                 print(f'no raidres found for {player}')
                 items = 'Nothing'
-
+            
             attendees_dict.update({player:items})
         else:
             not_attended_dict.update({player:items})
-            
+
     return attendees_dict,not_attended_dict
