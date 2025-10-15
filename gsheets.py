67a9c6505c251d3f91e5f8b78c8be8fd5abd2704
Gwynn-DevArt <10675768+JeSmyrna@users.noreply.github.com>
2025-10-15 10:34:15 +0200

    finalized gsheet export
    now clears doc via starting cell

--- gsheets.py
+++ gsheets.py
@@ -16,36 +16,26 @@ def gspread_overwrite(link:str,row_data:dict,start_cell:str = 'A1'):
     
     end_col_row = calc_sheet_length(data_no_nothing,start_cell)
     lists = []
-
     for entry in data_no_nothing:
         #ignore player who have no SR+
         lists.append(row_data[entry])
-    worksheet.update(lists, f'{start_cell}:{end_col_row}')
 
-#function is here just for testing
-def load_sr_sheet_data(filename:str):
-    data = rw_csv.load_sr_sheet(f'{filename}')
-        
-    return data
+    worksheet.batch_clear([f"A{start_cell[1]}:A50",f"B{start_cell[1]}:B50",f"C{start_cell[1]}:C50",f"D{start_cell[1]}:D50",f"E{start_cell[1]}:E50",f"F{start_cell[1]}:F50", f"G{start_cell[1]}:G50", f"H{start_cell[1]}:H50", f"I{start_cell[1]}:I50", f"J{start_cell[1]}:J50", f"K{start_cell[1]}:K50", f"L{start_cell[1]}:L50"])
+    worksheet.update(lists, f'{start_cell}:{end_col_row}')
 
 def calc_sheet_length( sr_sheet:dict, start_cell:str = 'A1'):
     start_cell_column = start_cell[0].lower()
     start_cell_row = int(start_cell[1])
-    #print(f'Starting Cell {start_cell}: {int(alphabet.index(start_cell_column))+1},{start_cell_row}')
 
     list_length = len(sr_sheet['columns'])-1
 
     to_column = alphabet.index(start_cell_column)+list_length
     to_row = len(sr_sheet) + start_cell_row -1
-    #print(f'{alphabet[to_column].capitalize()}{to_row}')
     end_row_col = f'{alphabet[to_column].capitalize()}{to_row}'
 
     return end_row_col
 
-def testing_input():
+def export_to_gsheet(sr_plus_sheet):
     entered_url = input('GSheet URL: ')
     entered_starting_cell = input('Starting Cell (Header:Player -> etc) like "A1": ')
-    data = load_sr_sheet_data('Test')
-    gspread_overwrite(entered_url,data,entered_starting_cell)
-
-testing_input()
+    gspread_overwrite(entered_url,sr_plus_sheet,entered_starting_cell)
