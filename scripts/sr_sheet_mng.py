16a4ce9086960bd4e718931668d0e04fd514db6c
Gwynn-DevArt <10675768+JeSmyrna@users.noreply.github.com>
2026-03-06 20:31:10 +0100

    SrSheetManager: added create new sr sheet function

--- scripts/sr_sheet_mng.py
+++ scripts/sr_sheet_mng.py
@@ -1,6 +1,6 @@
 from scripts.general_functions import get_date, color_text, print_loaded_file, order_dict_alphabetically
 from scripts.file_import import load_text_file, load_json, load_csv
-from scripts.export import save_json
+from scripts.export import save_json, save_csv
 from scripts.player_mng import PlayerManager
 
 import os, shutil
@@ -8,7 +8,7 @@ import os, shutil
 class SrSheetManager():
     def __init__(self):
         self.__directory = load_json("./Data/_config/sr_directory")[0]["name"]
-        self.__blueprint = [setting for setting in load_json("./Data/_config/config") if setting["name"] == "sr_sheet"]
+        self.__blueprint = [setting for setting in load_json("./Data/_config/config") if setting["name"] == "sr_sheet"][0]
         self.__sr_sheet: dict
         self.__player_dict: PlayerManager
     
@@ -23,7 +23,7 @@ class SrSheetManager():
             elif user_input == "1":
                 name_input = input("SR Sheet Name: ")
                 if self.__player_dict._ask_user(f"Create new SR Sheet {name_input}?"):
-                    pass
+                    self._create_new_sr_sheet(name_input)
                 else:
                     print("canceling...")
             else:
@@ -36,3 +36,26 @@ class SrSheetManager():
         for entry in menu:
             print(f"[{menu.index(entry)}] {entry}")
         return menu
+    
+    def save_sr_directory(self):
+        save_json("./Data/_config/sr_directory", [{"name":self.__directory}])
+
+    def _create_new_sr_sheet(self,raidname:str):
+
+        if os.path.exists(f"./Data/{raidname}"):
+            print("already exists")
+        else:
+            self.__directory.append(raidname)
+            self.save_sr_directory()
+
+            new_path = f"./Data/{raidname}/"
+            os.mkdir(new_path)
+
+            os.mkdir(f"{new_path}logs/")
+            os.mkdir(f"{new_path}sr_saves/")
+            os.mkdir(f"{new_path}sr_saves/sr_sheets/")
+            os.mkdir(f"{new_path}sr_saves/sr_awarded/")
+
+            save_json(f"{new_path}settings",[self.__blueprint["settings"]])
+            save_csv(f"{new_path}{raidname}",[self.__blueprint["columns"]])
+            save_csv(f"{new_path}sr_awarded",[self.__blueprint["awarded"]])
