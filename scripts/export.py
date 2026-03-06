cad4a29c962b69902d47dbf5a56a8a33b3115306
Gwynn-DevArt <10675768+JeSmyrna@users.noreply.github.com>
2026-03-06 20:30:33 +0100

    export func: added save to csv file
    made the write mode available

--- scripts/export.py
+++ scripts/export.py
@@ -1,4 +1,4 @@
-import json
+import json, csv
 
 def save_json(filename:str,data:list):
     path = f'{filename}.json'
@@ -15,6 +15,18 @@ def save_text(filename:str, text_data:list, override:bool = True):
     else:
         write_mode = "a"
 
-    with open(path, 'a') as sr_directory:
+    with open(path, write_mode) as sr_directory:
         for sheet_name in text_data:
             sr_directory.write(f'\n{sheet_name}')
+
+def save_csv(filepath:str,data:list):
+    with open(f'{filepath}.csv', 'w', newline='') as csvfile:
+        fieldnames = data[0]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for entry in data[1:]:
+            new_row = {}
+            for col in fieldnames:
+                new_row.update({col:entry[fieldnames.index(col)]})
+            writer.writerow(new_row)
