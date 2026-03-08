bbc9cca728515e7f090992d372bc887948ebf4d6
Gwynn-DevArt <10675768+JeSmyrna@users.noreply.github.com>
2026-03-08 21:44:52 +0100

    changed config file and added modes to csv export

--- scripts/export.py
+++ scripts/export.py
@@ -19,8 +19,14 @@ def save_text(filename:str, text_data:list, override:bool = True):
         for sheet_name in text_data:
             sr_directory.write(f'\n{sheet_name}')
 
-def save_csv(filepath:str,data:list):
-    with open(f'{filepath}.csv', 'w', newline='') as csvfile:
+def save_csv(filepath:str,data:list, override:bool = True):
+    write_mode = ""
+    if override:
+        write_mode = "w"
+    else:
+        write_mode = "a"
+    
+    with open(f'{filepath}.csv', write_mode, newline='') as csvfile:
         fieldnames = data[0]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
